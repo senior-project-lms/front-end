4d266dd514635895a7f327952104aa242172d685
UMIT KAS <umit.kas@outlook.com>
2018-01-20 17:55:58 +0300

    systemAnnouncment code comments

--- src/services/common.js
+++ src/services/common.js
@@ -6,6 +6,14 @@ const authService = new AuthenticationService();
 export default class Service{
     constructor(){}
 
+
+    /*
+        function: save
+        @param (path) api path for current process
+        @param (params) contains object which represents the pojo in backend
+        @return (boolean) returns boolean that represent saved or not
+        used in services, to save data, requests with POST method
+    */
     save(path, params){
         const accessToken = authService.getAccessToken();
         return Axios.post(`${path}?access_token=${accessToken}`, params)
@@ -23,7 +31,13 @@ export default class Service{
         })
     }
 
-    
+    /*
+        function: delete
+        @param (path) api path for current process
+        @param (publicKey) public key of deleted object
+        @return (boolean) returns boolean that represent deleted or not
+        used in services, to delete data, requests with DELETE method
+    */    
     delete(path, publicKey){
         const accessToken = authService.getAccessToken();
         return Axios.delete(`${path}/${publicKey}?access_token=${accessToken}`)
@@ -41,6 +55,14 @@ export default class Service{
         })
     }
 
+    /*
+        function: update
+        @param (path) api path for current process
+        @param (params) contains object which represents the pojo in backend
+        @param (publicKey) public key of updated object
+        @return (boolean) returns boolean that represent updated or not
+        used in services, to delete data, requests with PUT method
+    */    
     update(path, publicKey, params){
         const accessToken = authService.getAccessToken();
         return Axios.put(`${path}/${publicKey}?access_token=${accessToken}`, params)
@@ -57,7 +79,12 @@ export default class Service{
             return false;
         })
     }
-
+    /*
+        function: getAll
+        @param (path) api path for current process
+        @return (array) returns empty array, or array of the fetched objects from api
+        used in services, to fetch array of data, requests with GET method
+    */  
     getAll(path){
         const accessToken = authService.getAccessToken();
         return Axios.get(`${path}?access_token=${accessToken}`)
@@ -75,7 +102,13 @@ export default class Service{
         })
     }
 
-
+    /*
+        function: get
+        @param (path) api path for current process
+        @param (publicKey) public key of fetched object
+        @return (null or object) returns null or fetched object from api
+        used in services, to fetch single object from api and returns it, requests with GET method
+    */  
     get(path, publicKey){
         const accessToken = authService.getAccessToken();
         return Axios.get(`${path}/${publicKey}?access_token=${accessToken}`)
--- src/services/systemAnnouncement.js
+++ src/services/systemAnnouncement.js
@@ -7,14 +7,32 @@ export default class SystemAnnouncementService{
     constructor(){}
 
 
+    /*
+        function: save
+        @param (params) contains the system announncement object that is represented in backend as pojo
+        @return (boolean) returns boolean that represent saved or not
+        used for to save system announcement
+    */     
     save(params){
         return service.save('/api/admin/system-announcement', params);      
     }
 
+    /*
+        function: delete
+        @param (publicKey) public key is which represent the deleted object
+        @return (boolean) returns boolean that represent deleted or not
+        used for to delete system announcement
+    */       
     delete(publicKey){
         return service.delete('/api/admin/system-announcement', publicKey);
     }
 
+    /*
+        function: getAll
+        @param (page) page number, api returns 5 object for each page, increment page for new objects
+        @return (array) returns empty or filled array of announcment objects
+        used for the fetch system announcments page by page 
+    */       
     getAll(page){
         return service.getAll(`/api/system-announcements/${page}`);
     }
